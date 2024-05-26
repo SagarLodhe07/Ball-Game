1352e836d9118fb825272ca64b5bb0e07b23474c
Sagar Lodhe <156011005+SagarLodhe07@users.noreply.github.com>
2024-05-26 09:13:13 +0530

    Update main.js

--- main.js
+++ main.js
@@ -12,13 +12,6 @@ const color = [
   "cornflowerblue",
 ];
 
-// console.log(game_area);
-// for (let i = 0; i < 117; i++) {
-//   ball.classList.add("ball");
-//   ball.textContent = Math.floor(Math.random() * 10);
-//   ball.style.backgroundColor = color[Math.floor(Math.random() * color.length)];
-//   game_area.appendChild(ball);
-// }
 const game_area = document.querySelector("#game");
 console.log(getComputedStyle(game_area).getPropertyValue("width"));
 function create_Ball() {
